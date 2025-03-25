731b050f4af2edfd14af573c50c8b67e22636a3d
Paul Mayne <pmayne@gmail.com>
2025-03-25 13:45:04 -0600

    Initial Commit

---
+++ DO-AI-Controls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DO-AI-Controls
+//
+//  Created by Paul Mayne on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DO-AI-Controls/DO_AI_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DO_AI_ControlsApp.swift
+//  DO-AI-Controls
+//
+//  Created by Paul Mayne on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DO_AI_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
